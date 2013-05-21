79d421ced2d8072847ebee1079a2f5ae450a2cb0
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-05-21 12:35:22 -0500

    More setup

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,63 @@
 int main()
 {
 	sf::RenderWindow display {sf::VideoMode(854, 480), "Taylor Polynomials", sf::Style::Close};
-	while(true)
+	bool running {true};
+	while(running)
 	{
+		sf::Event e;
+		bool click {false}
+		,    w     {false}
+		,    a     {false}
+		,    s     {false}
+		,    d     {false};
+		int mx {0}
+		,   my {0};
 		while(display.pollEvent(event))
 		{
 			//handle event
+			switch(e.type)
+			{
+			case sf::Event:Closed:
+				{
+					running = false;
+				} break;
+			case sf::Event::KeyPressed:
+				{
+					switch(e.key.code)
+					{
+						//
+					}
+				} break;
+			case sf::Event::KeyReleased:
+				{
+					switch(e.key.code)
+					{
+						//
+					}
+				} break;
+			case sf::Event::MouseButtonPressed:
+				{
+					if(e.mouseButton.buton == sf::Mouse::Left)
+					{
+						click = true;
+					}
+				} break;
+			case sf::Event::MouseButtonReleased:
+				{
+					if(e.mouseButton.buton == sf::Mouse::Left)
+					{
+						click = false;
+					}
+				} break;
+			case sf::Event::MouseMoved:
+				{
+					mx = e.mouseMove.x;
+					my = e.mouseMove.y;
+				}
+			}
 		}
+
+		//logic
 
 		//render
 		display.display();
